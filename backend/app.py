1471562f55d730e4428d4613f3f84e2c517f107e
ErtKid <avrit.max@gmail.com>
2024-02-01 15:50:11 +0100

    Config dockerfile

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template,request,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder='../frontend/templates', static_folder='../frontend/static')
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///db.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://user:password@db/tododb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db= SQLAlchemy(app)
 
